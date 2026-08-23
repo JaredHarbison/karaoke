107834a7e835b6d9b8ba9bff7e663869aa2896b4
Jared Harbison <jared.harbison@gmail.com>
2026-08-23 11:29:56 -0400

    fix(ui): animate shell flash messages

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,10 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.timeout = setTimeout(() => this.element.remove(), 4000)
+    this.timeout = setTimeout(() => {
+      this.element.classList.add("is-exiting")
+      this.removeTimeout = setTimeout(() => this.element.remove(), 100)
+    }, 3900)
   }
 
   disconnect() {
     clearTimeout(this.timeout)
+    clearTimeout(this.removeTimeout)
   }
 }
